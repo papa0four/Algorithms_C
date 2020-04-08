6027e74ba68ecd018afc19df4c603bbc564c6492
papa0four <papa.o.deuce@gmail.com>
2020-04-07 22:30:10 -0400

    created a doubly-linked list 'michael_duble' that appends a node, prints forward and reverse list traversal, delete_node and destroy list

--- LinkedLists/michael_duble.c
+++ LinkedLists/michael_duble.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-//#include <string.h>
 
-typedef struct link_node
+typedef struct node
 {
 	int data;
-	struct link_node* next;
+	struct node* prev;
+	struct node* next;
 } Node;
 
-typedef struct linked_list
+typedef struct link_list
 {
 	Node* head_node;
 } List;
@@ -19,11 +19,12 @@ Node* new_node (int data)
 	Node* new = (Node *)malloc(sizeof(Node));
 	if (new == NULL)
 	{
-		perror("Could not allocate memory for new in new_node");
+		perror("Could not allocalte memory for new in new_node");
 		exit(EXIT_FAILURE);
 	}
 
 	new->data = data;
+	new->prev = NULL;
 	new->next = NULL;
 
 	return new;
@@ -32,7 +33,7 @@ Node* new_node (int data)
 List* list_init ()
 {
 	List* lHead = (List *)malloc(sizeof(List));
-	if (lHead == NULL)
+	if(lHead == NULL)
 	{
 		perror("Could not allocate memory for lHead in list_init");
 		exit(EXIT_FAILURE);
@@ -43,12 +44,12 @@ List* list_init ()
 	return lHead;
 }
 
-bool append (List* lHead, int data)
+bool append (List* lHead,  int data)
 {
 	Node* hNode = new_node(data);
 	if (hNode == NULL)
 	{
-		perror("Could not allocate memory for hNode in append");
+		perror("Could not create hNode in append");
 		exit(EXIT_FAILURE);
 	}
 
@@ -66,27 +67,38 @@ bool append (List* lHead, int data)
 	}
 
 	current->next = hNode;
+	hNode->prev = current;
+
 	return true;
 }
 
-void print_list (List* link_list)
+void print_list (List* lHead)
 {
-	if (link_list->head_node == NULL)
-	{
-		printf("No list to print\n");
-		return;
-	}
+	Node* current = lHead->head_node;
+	current->prev = NULL;
 
-	Node* current;
-	current = link_list->head_node;
-
-	while (current)
+	printf("Printing forward traversal:\n");
+	while (current->next != NULL)
 	{
-		printf("Data in node: %d\n", current->data);
+		printf("Node %d ", current->data);
 		current = current->next;
 	}
+	if (current->next == NULL)
+	{
+		printf("Node %d\n", current->data);
+	}
 
-	return;
+	printf("\nPrinting reverse traversal:\n");
+	while(current->prev != NULL)
+	{
+		printf("Node %d ", current->data);
+		current = current->prev;
+	}
+
+	if (current->prev == NULL)
+	{
+		printf("Node %d\n", current->data);
+	}
 }
 
 bool search (List* lHead, int data)
@@ -104,14 +116,16 @@ bool search (List* lHead, int data)
 		current = current->next;
 	}
 
+	//if searched node is tail
 	if (data == current->data)
 	{
 		printf("Node %d found!\n", data);
-		return true;
 	}
-
-	printf("Node %d does not exist in list\n", data);
-	return false;
+	else
+	{
+		printf("Node %d does not exist in list\n", data);
+		return false;
+	}
 }
 
 Node* remove_node (List* lHead, int data)
@@ -124,20 +138,30 @@ Node* remove_node (List* lHead, int data)
 	{
 		printf("Deleting head node: %d\n", data);
 		lHead->head_node = temp->next;
+		lHead->head_node->prev = NULL;
 		return temp;
 	}
+
 
 	//loop through list to search for selected node and keep track or previous node
 	while (temp != NULL && data != temp->data)
 	{
+		temp->prev = previous;
 		previous = temp;
-		temp = temp->next;
+		temp = temp->next;		
 	}
 
 	if (temp == NULL)
 	{
 		printf("Node %d does not exist in list\n", data);
 		return NULL;
+	}
+
+	if (temp->next == NULL && data == temp->data)
+	{
+		printf("Deleting tail node: %d\n", data);
+		previous->next = NULL;
+		return temp;
 	}
 
 	//remove node from the list
@@ -165,38 +189,42 @@ void destroy_list (List* lHead)
 int main ()
 {
 	List* link_list = list_init();
+
 	Node* node;
 
 	bool check;
 
-	for (int i = 10; i <= 50; i += 10)
+	for (int i = 10; i <= 100; i += 10)
 	{
 		check = append(link_list, i);
 
 		if (!check)
 		{
-			printf("LIST NODE HAS CONTRACTED COVID19, ERADICATE IMMEDIATELY!\n");
+			printf("LIST HAS CONTRACTED COVID19, PLEASE PERFORM ERADICATION PROCEDURES!\n");
 		}
 	}
 
-	search(link_list, 40);
-	search(link_list, 50);
-	search(link_list, 100);
-
 	print_list(link_list);
 
-	node = remove_node(link_list, 10);
-	free(node);
-	node = remove_node(link_list, 30);
+	search(link_list, 10);
+	search(link_list, 40);
+	search(link_list, 250);
+	search(link_list, 70);
+	search(link_list, 100);
+
+	node = remove_node(link_list, 80);
 	free(node);
 	node = remove_node(link_list, 50);
 	free(node);
-	node = remove_node(link_list, 80);
+	search(link_list, 50);
+	node = remove_node(link_list, 100);
 	free(node);
-
+	node = remove_node(link_list, 10);
+	free(node);
+	search(link_list, 10);
 	print_list(link_list);
 
-	printf("\nDeleting list...\n");
+	printf("Destroying list...\n");
 	destroy_list(link_list);
 
 	return 0;
